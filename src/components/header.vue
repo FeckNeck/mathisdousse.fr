9e0707de9d2a693d6711c69b13091ce4df02e287
feckneck <math.dousse@hotmail.fr>
2024-04-29 19:54:06 +0200

    feat(header): add responsive and logos

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,12 +13,19 @@ const nextTheme = computed(() => (isDark.value ? 'clair' : 'sombre'));
   <header>
     <div class="container">
       <div class="d-flex items-center justify-between py-8">
-        <img src="../assets/images/logo.png" alt="MD logo" />
-        <div class="d-flex items-center g-2">
+        <img
+          src="../assets/images/logo.png"
+          alt="Mathis Dousse logo"
+          class="logo" />
+        <div class="d-flex items-center g-2 justify-between">
           <a class="mail-btn rounded" href="mailto:mathis.dousse@outlook.fr">
             <Mail :size="16" />
             <span>Me contacter</span>
           </a>
+          <img
+            src="../assets/images/logo_short.png"
+            alt="Mathis Dousse logo"
+            class="logo_short" />
           <button
             class="theme-switch"
             @click="toggleDark()"
@@ -33,32 +40,70 @@ const nextTheme = computed(() => (isDark.value ? 'clair' : 'sombre'));
 </template>
 
 <style scoped lang="scss">
-.mail-btn {
-  display: flex;
-  align-items: center;
-  padding: 0.5rem 1rem;
-  background-color: var(--teal);
-  border-radius: var(--rounded-sm);
-  color: white;
-  border: none;
-  gap: 0.5rem;
-  cursor: pointer;
+header {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  backdrop-filter: blur(10px);
+  height: var(--header);
 
-  &:hover {
-    filter: brightness(0.9);
+  .logo {
+    width: 7rem;
   }
-}
 
-.theme-switch {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  color: inherit;
-  opacity: 0.7;
-  transition: opacity 0.3s;
+  .logo_short {
+    display: none;
+    width: 3.5rem;
+  }
 
-  &:hover {
-    opacity: 1;
+  .mail-btn {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    background-color: var(--teal);
+    border-radius: var(--rounded-sm);
+    color: white;
+    border: none;
+    gap: 0.5rem;
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+
+  .theme-switch {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: inherit;
+    opacity: 0.7;
+    transition: opacity 0.3s ease;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  @media (max-width: 768px) {
+    div:nth-child(2) {
+      flex-grow: 1;
+    }
+
+    .logo {
+      display: none;
+    }
+
+    .logo_short {
+      display: block;
+    }
+
+    .mail-btn {
+      > span {
+        display: none;
+      }
+    }
   }
 }
 </style>
