688edff07118f94290fec1c325f32bd1baaa8aad
feckneck <math.dousse@hotmail.fr>
2024-05-03 09:53:25 +0200

    feat: add footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,9 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Linkedin, Github } from 'lucide-vue-next';
+
+const currentYear = new Date().getFullYear();
+</script>
 
 <template>
-  <footer class="container">
-    <h1>Footer</h1>
+  <footer>
+    <div class="container">
+      <div class="d-flex items-center justify-between py-8">
+        <p>&copy; {{ currentYear }} - Mathis Dousse</p>
+        <div class="d-flex items-center g-4">
+          <a
+            href="https://www.linkedin.com/in/mathis-dousse/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <Linkedin :size="20" />
+          </a>
+          <a
+            href="https://github.com/FeckNeck"
+            target="_blank"
+            rel="noopener noreferrer">
+            <Github :size="20" />
+          </a>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+footer {
+  background-color: var(--gray-800);
+  border: 2px solid var(--gray-700);
+
+  a {
+    transition: color 0.3s ease;
+  }
+
+  a:hover {
+    color: var(--teal);
+  }
+}
+</style>
